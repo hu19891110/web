db38a1062e7045ecb88e5663dae5ab21fe32c0af
liuzheng712 <liuzheng712@gmail.com>
2016-04-12 08:42:35 +0800

    main.ts fix

--- static/ts/main.ts
+++ static/ts/main.ts
@@ -3,9 +3,59 @@
  */
 
 import {bootstrap}        from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS} from 'angular2/router';
-import {HTTP_PROVIDERS}   from 'angular2/http';
-import {AppComponent}     from './app.component';
+import {Http, HTTP_PROVIDERS}   from 'angular2/http';
+import {Component}         from 'angular2/core';
+import {ROUTER_PROVIDERS, RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
+import  'rxjs/Rx';
+declare var jQuery:any;
+
+import {LoginComponent} from './login';
+import {TermComponent} from './terminal';
+import {DashboardComponent} from './dashboard';
+
+@Component({
+    template: '',
+})
+export class IndexComponent {
+}
+
+@Component({
+    selector: 'angular2',
+    template: `<router-outlet></router-outlet>`,
+    directives: [ROUTER_DIRECTIVES]
+})
+
+@RouteConfig([
+    {path: '/', name: 'Index', component: IndexComponent, useAsDefault: true},
+    {path: '/login', name: 'Login', component: LoginComponent},
+    {path: '/terminal', name: 'Terminal', component: TermComponent},
+    {path: '/dashboard', name: 'Dashboard', component: DashboardComponent},
+])
+
+
+export class AppComponent {
+    constructor(private http:Http,
+                private _router:Router) {
+    }
+
+    ngOnInit() {
+        this.http.get('/checklogin')
+            .map(res => res.json())
+            .subscribe(
+                data => this.data = data,
+                err => this.logError(err),
+                () => {
+                    if (this.data.logined) {
+                        jQuery('body').addClass('logined');
+                        this._router.navigate(['Dashboard']);
+                    } else {
+                        this._router.navigate(['Login']);
+                    }
+                    jQuery('angular2').show();
+                }
+            );
+    }
+}
 
 bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
