dcdd4d4008a0a6c229aefce1bc383436b577cf9b
liuzheng712 <liuzheng712@gmail.com>
2016-04-12 09:57:55 +0800

    mv main.ts to index.ts

--- static/ts/index.ts
+++ static/ts/index.ts
@@ -2,47 +2,62 @@
  * Created by liuzheng on 4/7/16.
  */
 
-import {Component} from 'angular2/core';
-import {Http, HTTP_PROVIDERS, Headers, Response} from 'angular2/http';
-import {RouteParams, Router, ROUTER_DIRECTIVES} from 'angular2/router';
+import {bootstrap}        from 'angular2/platform/browser';
+import {Http, HTTP_PROVIDERS}   from 'angular2/http';
+import {Component}         from 'angular2/core';
+import {ROUTER_PROVIDERS, RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
 import  'rxjs/Rx';
 declare var jQuery:any;
 
+import {LoginComponent} from './login';
+import {TermComponent} from './terminal';
+import {DashboardComponent} from './dashboard';
+
 @Component({
     template: '',
+})
+export class IndexComponent {
+}
+
+@Component({
+    selector: 'angular2',
+    template: `<router-outlet></router-outlet>`,
     directives: [ROUTER_DIRECTIVES]
 })
 
-export class IndexComponent {
+@RouteConfig([
+    {path: '/', name: 'Index', component: IndexComponent, useAsDefault: true},
+    {path: '/login', name: 'Login', component: LoginComponent},
+    {path: '/terminal', name: 'Terminal', component: TermComponent},
+    {path: '/dashboard', name: 'Dashboard', component: DashboardComponent},
+])
+
+
+export class AppComponent {
     constructor(private http:Http,
                 private _router:Router) {
     }
 
     ngOnInit() {
-        var csrftoken = jQuery('meta[name=csrf-token]').attr('content');
-        var authHeader:Headers = new Headers();
-        authHeader.append('Content-Type', 'application/x-www-form-urlencoded');
-        if (csrftoken) {
-            authHeader.append('X-CSRFToken', csrftoken);
-        }
-
         this.http.get('/checklogin')
             .map(res => res.json())
             .subscribe(
-                data => {
-                    this.data = data
-                },
+                data => this.data = data,
                 err => this.logError(err),
                 () => {
                     if (this.data.logined) {
                         jQuery('body').addClass('logined');
-                        this._router.parent.navigate(['Terminal']);
-                    }else{
-                        this._router.parent.navigate(['Login']);
+                        this._router.navigate(['Dashboard']);
+                    } else {
+                        this._router.navigate(['Login']);
                     }
+                    jQuery('angular2').show();
                 }
             );
     }
 }
 
-
+bootstrap(AppComponent, [
+    ROUTER_PROVIDERS,
+    HTTP_PROVIDERS
+]);
