25be3d6372d6b5174431dff4efea9a0f9138036f
liuzheng712 <liuzheng712@gmail.com>
2016-05-02 10:16:26 +0800

    angular2-cookie

--- static/ts/service.ts
+++ static/ts/service.ts
@@ -7,6 +7,7 @@ import {Http, HTTP_PROVIDERS}   from 'angular2/http';
 import {ROUTER_PROVIDERS, RouteConfig, Router, ROUTER_DIRECTIVES} from 'angular2/router';
 // import {Observable} from 'rxjs/Observable';
 // import {Observer} from 'rxjs/Observer';
+import {CookieService} from 'angular2-cookie/core';
 import {Logger} from "angular2-logger/core";
 // import {DynamicRouteConfigurator} from './dynamicRouteConfigurator'
 import 'rxjs/add/operator/share';
@@ -54,19 +55,25 @@ export class AppService {
 
     constructor(private http:Http,
                 private _router:Router,
-                private _logger:Logger) {
+                private _logger:Logger,
+                private _cookie:CookieService) {
+        if (this._cookie.get('loglevel'))
         // 0.- Level.OFF
         // 1.- Level.ERROR
         // 2.- Level.WARN
         // 3.- Level.INFO
         // 4.- Level.DEBUG
         // 5.- Level.LOG
-        this._logger.level = 5;
+            this._logger.level = this._cookie.get('loglevel');
         // this._logger.debug('Your debug stuff');
         // this._logger.info('An info');
         // this._logger.warn('Take care ');
         // this._logger.error('Too late !');
         // this._logger.log('log !');
+        else {
+            this._cookie.put('loglevel', 0);
+            this._logger.level = 0
+        }
     }
 
     setLoglevel(level:number) {
@@ -85,7 +92,7 @@ export class AppService {
                     var route = DataStore.lastNavigationAttempt.match(RegExp(DataStore.Path['regex']));
                     var key = DataStore.Path['path'].match(RegExp(DataStore.Path['regex']));
                     DataStore.Path['res'] = {};
-                    key.map((v,k) => {
+                    key.map((v, k) => {
                         if (typeof k === 'number' && k > 0)
                             DataStore.Path['res'][v] = route[k]
                     });
